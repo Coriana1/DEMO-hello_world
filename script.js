50830c814f38e3189fef034c7071a88de8c8fcac
Coriana Williams <corianacory@gmail.com>
2022-10-15 01:02:44 -0400

    Updated more

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-'Use strict';
+
+
+function greetUser(){
+    let greetUser = prompt("Heyyy, What is your name?");
+    document.write("Hi" + greetUser + "I hope you enjoy my page");
+    return greetUser;
+}
+
+
+let lastMemory = prompt("Was the last time you ate cake for a birthday celebration?");
+if (lastMemory == "yes") { 
+  document.write("I LOVE BIRTHDAY CAKE!");
+}  else if (lastMemory == "no") {
+  document.write("I still hope it was delicious!");
+}
